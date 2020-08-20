d335772a3173a28bccf84f1b06d8bbd063fcd7ca
pipe473 <bedoyafelipe743@gmail.com>
2020-08-20 16:03:48 +0200

    Añadiendo modificaciones

--- main.ts
+++ main.ts
@@ -5,3 +5,4 @@ import { profesiones } from './IMDB';
 let actor = new Professional("Desiree", 26, "Drama", 65, 1.70, "Castaño", "Marrón", "Escritora", false, "Española", 6, profesiones.actor);
 
 actor.showAttributes();
+
