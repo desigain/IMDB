98064f83ae23b094c21b5d33fe8944c413111bd8
pipe473 <bedoyafelipe743@gmail.com>
2020-08-20 17:43:22 +0200

    Creando testing para verb que los atributos funcionan

--- mainIMDB.ts
+++ mainIMDB.ts
@@ -1,1 +1,13 @@
+import {IMDB} from './IMDB';
+import {Movie} from './movie';
+
+let movieOne = new Movie("Frozen II", 2020, "EEUU", "Infantil");
+let movieTwo = new Movie("Rambo", 1998, "China", "Acción");
+let totalMovie = new IMDB([movieOne, movieTwo]);
+
+console.log(totalMovie.peliculas[0].nacionality);
+console.log(totalMovie.peliculas[1].showMovies());
+console.log(totalMovie.peliculas[0].genre);
+
+
 
