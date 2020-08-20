faaec1ad4974a07e8ce77bc96cddd6c566ac8674
pipe473 <bedoyafelipe743@gmail.com>
2020-08-20 17:18:17 +0200

    Modificacion de la ruta de importacion de profesional

--- mainProfessional.ts
+++ mainProfessional.ts
@@ -1,5 +1,5 @@
-import { Professional } from './IMDB';
-import { profesiones } from './IMDB';
+import { Professional } from './professional';
+import { profesiones } from './professional';
 
 
 let actor = new Professional("Desiree", 26, "Drama", 65, 1.70, "Castaño", "Marrón", "Escritora", false, "Española", 6, profesiones.actor);
