165ec58428089af36f17782b1173b15fb3f6ee32
pipe473 <bedoyafelipe743@gmail.com>
2020-08-21 10:12:51 +0200

    Ultima modificacion de la clase IMDB

--- IMDB.ts
+++ IMDB.ts
@@ -1,5 +1,4 @@
 //Crear una clase denominada IMDB cuyo constructor reciba un array de objetos de la clase Movie
-<<<<<<< HEAD
 import {Movie} from "./movie"
 
   export class IMDB 
@@ -13,18 +12,5 @@ import {Movie} from "./movie"
       this.peliculas = peliculas;
     }
   }
-=======
-import { Movie } from "./movie";
-
-export class IMDB {
-  //Declaración de atributos o propiedades
-  public peliculas: Movie[];
-
-  //Implementación del método constructor
-  constructor(peliculas: Movie[]) {
-    this.peliculas = peliculas;
-  }
-}
 
 
->>>>>>> c6f7f905d53ed88155690e3284e2df2a8ee0c325
