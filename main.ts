eeaafc02955c3a8383113aa7923ee253a95cc535
pipe473 <bedoyafelipe743@gmail.com>
2020-08-24 10:43:23 +0200

    Creando peliculas añadiendo valores a sus atributos por medio de las funciones del readline

--- main.ts
+++ main.ts
@@ -1,8 +1,5 @@
 import { IMDB } from "./IMDB";
 import { Movie } from "./movie";
-// import { Professional } from "./professional";
-// import { profesiones } from "./professional";
-const fs = require("file-system");
 
 const readline = require("readline").createInterface({
   input: process.stdin,
@@ -10,30 +7,33 @@ const readline = require("readline").createInterface({
 });
 
 readline.question("Titulo: ", function (title: string) {
-  // let test = title;
+  readline.question("Año: ", function (releaseYear: number) {
+    readline.question("País: ", function (nacionality: string) {
+      readline.question("Género: ", function (genre: string) {
+        let movie1 = new Movie(title, releaseYear, nacionality, genre);
+        let movie2 = new Movie(title, releaseYear, nacionality, genre);
 
-  let movie1 = new Movie(title, 3, "aaa646", "drama");
-  let movie2 = new Movie(title, 6, "jajjajaja", "comica");
-  // console.log(movie1);
+        // console.log(movie1);
 
-  let objetoPrueba: IMDB = new IMDB([]);
+        let objetoPrueba: IMDB = new IMDB([]);
 
-  // //   objetoPrueba.escribirJSON("prueba.json");
+        // //   objetoPrueba.escribirJSON("prueba.json");
 
-  objetoPrueba.obtenerInstancia("prueba.json");
-  console.log(objetoPrueba);
-  console.log(objetoPrueba.peliculas);
+        objetoPrueba.obtenerInstancia("prueba.json");
+        console.log(objetoPrueba);
+        console.log(objetoPrueba.peliculas);
 
-  objetoPrueba.peliculas.push(movie1);
+        objetoPrueba.peliculas.push(movie1);
 
-  console.log(objetoPrueba);
+        console.log(objetoPrueba);
 
-  objetoPrueba.escribirJSON("prueba.json");
+        objetoPrueba.escribirJSON("prueba.json");
 
-  //   console.log(infoJSON);
+        //   console.log(infoJSON);
 
-  //   objetoPrueba.escribirJSON("prueba.json");
-  readline.close();
+        //   objetoPrueba.escribirJSON("prueba.json");
+        readline.close();
+      });
+    });
+  });
 });
-
-
